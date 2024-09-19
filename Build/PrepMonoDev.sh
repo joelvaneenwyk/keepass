7bcf6c7602e0554e6755735869fa70e3e974d83d
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-09-19 09:37:45 -0700

    Reformat shell script
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- Build/PrepMonoDev.sh
+++ Build/PrepMonoDev.sh
@@ -10,52 +10,48 @@ kpIcoG="${kpRoot}/Ext/Icons_15_VA/LowResIcons/KeePass_LR_G.ico"
 kpIcoR="${kpRoot}/Ext/Icons_15_VA/LowResIcons/KeePass_LR_R.ico"
 kpIcoY="${kpRoot}/Ext/Icons_15_VA/LowResIcons/KeePass_LR_Y.ico"
 
-fnPrepSolution()
-{
-	cd "${kpRoot}"
-	local kpSln="KeePass.sln"
+fnPrepSolution() {
+    cd "${kpRoot}" || exit
+    kpSln="KeePass.sln"
 
-	# Update solution format to 11 (this targets Mono 4 rather than 3.5)
-	sed -i 's!Format Version 10\.00!Format Version 11\.00!g' "${kpSln}"
+    # Update solution format to 11 (this targets Mono 4 rather than 3.5)
+    sed -i 's!Format Version 10\.00!Format Version 11\.00!g' "${kpSln}"
 }
 
-fnPrepKeePass()
-{
-	cd "${kpRoot}/KeePass"
-	local kpCsProj="KeePass.csproj"
+fnPrepKeePass() {
+    cd "${kpRoot}/KeePass" || exit
+    kpCsProj="KeePass.csproj"
 
-	sed -i 's! ToolsVersion="3\.5"!!g' "${kpCsProj}"
-	sed -i 's!<SignAssembly>true</SignAssembly>!<SignAssembly>false</SignAssembly>!g' "${kpCsProj}"
-	sed -i '/sgen\.exe/d' "${kpCsProj}"
+    sed -i 's! ToolsVersion="3\.5"!!g' "${kpCsProj}"
+    sed -i 's!<SignAssembly>true</SignAssembly>!<SignAssembly>false</SignAssembly>!g' "${kpCsProj}"
+    sed -i '/sgen\.exe/d' "${kpCsProj}"
 
-	cp -f "${kpIco}" KeePass.ico
-	cp -f "${kpIco}" Resources/Icons/KeePass.ico
-	cp -f "${kpIcoG}" Resources/Icons/KeePass_G.ico
-	cp -f "${kpIcoR}" Resources/Icons/KeePass_R.ico
-	cp -f "${kpIcoY}" Resources/Icons/KeePass_Y.ico
+    cp -f "${kpIco}" KeePass.ico
+    cp -f "${kpIco}" Resources/Icons/KeePass.ico
+    cp -f "${kpIcoG}" Resources/Icons/KeePass_G.ico
+    cp -f "${kpIcoR}" Resources/Icons/KeePass_R.ico
+    cp -f "${kpIcoY}" Resources/Icons/KeePass_Y.ico
 }
 
-fnPrepKeePassLib()
-{
-	cd "${kpRoot}/KeePassLib"
-	local kpCsProj="KeePassLib.csproj"
-	local kpXmlUtilEx="Utility/XmlUtilEx.cs"
+fnPrepKeePassLib() {
+    cd "${kpRoot}/KeePassLib" || exit
+    kpCsProj="KeePassLib.csproj"
+    kpXmlUtilEx="Utility/XmlUtilEx.cs"
 
-	sed -i 's! ToolsVersion="3\.5"!!g' "${kpCsProj}"
-	sed -i 's!<SignAssembly>true</SignAssembly>!<SignAssembly>false</SignAssembly>!g' "${kpCsProj}"
+    sed -i 's! ToolsVersion="3\.5"!!g' "${kpCsProj}"
+    sed -i 's!<SignAssembly>true</SignAssembly>!<SignAssembly>false</SignAssembly>!g' "${kpCsProj}"
 
-	sed -i -E 's!(xrs\.ProhibitDtd = true;)!// \1!g' "${kpXmlUtilEx}"
-	sed -i -E 's!// (xrs\.DtdProcessing = DtdProcessing\.Prohibit;)!\1!g' "${kpXmlUtilEx}"
+    sed -i -E 's!(xrs\.ProhibitDtd = true;)!// \1!g' "${kpXmlUtilEx}"
+    sed -i -E 's!// (xrs\.DtdProcessing = DtdProcessing\.Prohibit;)!\1!g' "${kpXmlUtilEx}"
 }
 
-fnPrepTrlUtil()
-{
-	cd "${kpRoot}/Translation/TrlUtil"
-	local kpCsProj="TrlUtil.csproj"
+fnPrepTrlUtil() {
+    cd "${kpRoot}/Translation/TrlUtil" || exit
+    kpCsProj="TrlUtil.csproj"
 
-	sed -i 's! ToolsVersion="3\.5"!!g' "${kpCsProj}"
+    sed -i 's! ToolsVersion="3\.5"!!g' "${kpCsProj}"
 
-	cp -f "${kpIco}" Resources/KeePass.ico
+    cp -f "${kpIco}" Resources/KeePass.ico
 }
 
 fnPrepSolution
@@ -63,4 +59,4 @@ fnPrepKeePass
 fnPrepKeePassLib
 fnPrepTrlUtil
 
-cd "${kpBuild}"
+cd "${kpBuild}" || exit 5
